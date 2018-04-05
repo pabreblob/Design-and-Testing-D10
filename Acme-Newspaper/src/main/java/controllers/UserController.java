e4786040de36d5838ebbf45ec435509efd17c26a
Daniel <danielcarpioc@gmail.com>
2018-04-05 10:09:21 +0200

    Crear usuarios, tags modificados y security

--- Acme-Newspaper/src/main/java/controllers/UserController.java
+++ Acme-Newspaper/src/main/java/controllers/UserController.java
@@ -5,12 +5,14 @@ import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import services.UserService;
 import domain.User;
+import forms.UserForm;
 
 @Controller
 @RequestMapping("/user")
@@ -30,6 +32,58 @@ public class UserController extends AbstractController {
 		result = new ModelAndView("user/list");
 		result.addObject("users", users);
 
+		return result;
+	}
+
+	@RequestMapping(value = "/create", method = RequestMethod.GET)
+	public ModelAndView create() {
+		ModelAndView result;
+		final UserForm userForm = new UserForm();
+
+		result = new ModelAndView("user/edit");
+		result.addObject("userForm", userForm);
+
+		return result;
+	}
+
+	@RequestMapping(value = "/save", method = RequestMethod.POST, params = "save")
+	public ModelAndView save(final UserForm userForm, final BindingResult binding) {
+		ModelAndView result;
+		final User user = this.userService.reconstruct(userForm, binding);
+		if (binding.hasErrors()) {
+			result = this.createEditModelAndView(userForm);
+			System.out.println("Error general");
+			System.out.println(binding.getAllErrors());
+		} else if (userForm.isAcceptTerms() == false) {
+			result = this.createEditModelAndView(userForm, "user.notAccepted.error");
+			System.out.println("No aceptado los términos");
+		} else if (!(userForm.getConfirmPass().equals(userForm.getUserAccount().getPassword()))) {
+			result = this.createEditModelAndView(userForm, "user.differentPass.error");
+			System.out.println("Contraseñas distintas");
+		} else
+			try {
+				this.userService.save(user);
+				result = new ModelAndView("redirect:/welcome/index.do");
+			} catch (final Throwable oops) {
+				result = this.createEditModelAndView(userForm, "user.commit.error");
+				System.out.println("Error grave");
+				System.out.println(oops.getMessage());
+			}
+		return result;
+
+	}
+
+	protected ModelAndView createEditModelAndView(final UserForm userForm) {
+		final ModelAndView result;
+		result = this.createEditModelAndView(userForm, null);
+		return result;
+	}
+
+	protected ModelAndView createEditModelAndView(final UserForm userForm, final String messageCode) {
+		final ModelAndView result;
+		result = new ModelAndView("user/edit");
+		result.addObject("userForm", userForm);
+		result.addObject("message", messageCode);
 		return result;
 	}
 }
