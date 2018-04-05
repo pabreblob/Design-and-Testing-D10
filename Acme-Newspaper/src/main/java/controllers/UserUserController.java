26a0c962a774341333a26f0dddb2bade5e8cc499
Daniel <danielcarpioc@gmail.com>
2018-04-05 15:39:42 +0200

    Ya se puede listar a los usuario que sigues y los que te siguen

---
+++ Acme-Newspaper/src/main/java/controllers/UserUserController.java
@@ -0,0 +1,40 @@
+
+package controllers;
+
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import services.UserService;
+import domain.User;
+
+@Controller
+@RequestMapping("/user/user")
+public class UserUserController extends AbstractController {
+
+	@Autowired
+	private UserService	userService;
+
+
+	@RequestMapping(value = "followers", method = RequestMethod.GET)
+	public ModelAndView listFollowers() {
+		ModelAndView result;
+		final Collection<User> followers = this.userService.findByPrincipal().getFollowers();
+		result = new ModelAndView("user/followers");
+		result.addObject("users", followers);
+		return result;
+	}
+
+	@RequestMapping(value = "following", method = RequestMethod.GET)
+	public ModelAndView listFollowing() {
+		ModelAndView result;
+		final Collection<User> following = this.userService.findByPrincipal().getFollowing();
+		result = new ModelAndView("user/following");
+		result.addObject("users", following);
+		return result;
+	}
+}
